47633e8146fb97def5a663fd305a43b3a6126d2f
Lucas <Lucas@clase>
2022-03-28 21:28:57 +0200

    RetoCesarPRO CONSEGUIDO

--- UD9/src/EjsB/RetoCesarPRO.java
+++ UD9/src/EjsB/RetoCesarPRO.java
@@ -3,13 +3,18 @@ package EjsB;
 import java.io.*;
 import java.util.*;
 
-public class RetoCesarMID {
+public class RetoCesarPRO {
     public static int clave;
         
+    /**
+     * MAIN del programa :)
+     * @param args 
+     */
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         ArrayList<String> palabras = new ArrayList();
         String palabra;
+        String textoCompleto = "";
         
         //Preguntamos por el texto a encriptar y creamos un File para dicho archivo
         System.out.println("¿Cómo se llama el archivo a desencriptar?");
@@ -31,17 +36,11 @@ public class RetoCesarMID {
          */
         
         try {
-            
-            //Preguntamos por la clave a utilizar. Está dentro del try-catch para evitar el inputMismatchException.
-            System.out.println("Cuál clave será utilizada?");
-            clave = leer.nextInt();
-            leer.nextLine(); //para atrapar el enter de la anterior lectura.
-            
-            
             Scanner in = new Scanner(cifrado);
             while (in.hasNext()){
                 palabra = in.next();
-                palabras.add(tradPalabra(palabra));
+                textoCompleto = textoCompleto + palabra + " ";
+                palabras.add(palabra);
             }
             in.close();
         } catch (Exception e) {
@@ -53,13 +52,17 @@ public class RetoCesarMID {
          */
         System.out.println("Qué nombre tendrá el archivo con la carta descifrada?");
         String nombre = leer.nextLine();
+        
+        //Vemos qué letra es la más repetida. Restamos el valor de dicha letra
+        //al de la primera letra del abecedario (A) y así averiguamos la clave.
+        clave = ((int)letraMasFrecuente(textoCompleto) - (int)'A');
         //Creamos el archivo
         File descifrado = new File(nombre);
         try {
             FileWriter escribir = new FileWriter(descifrado, true);
             for (int i = 0; i < palabras.size(); i++) {
-                escribir.write(palabras.get(i) + " ");
-            }      
+                escribir.write(tradPalabra(palabras.get(i)) + " ");
+            }
             escribir.close();
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -99,5 +102,33 @@ public class RetoCesarMID {
         } else if ((int) car < 65 || (int)car > 90+claveReal){
         }
         return carNuevo;
+    }
+    
+    public static char letraMasFrecuente(String texto) {
+        char[] vchars = texto.toUpperCase().toCharArray();
+        char[] letrasAbeced = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        int[] repetCadaLetra = new int[letrasAbeced.length]; //La posición del array corresponde a la posicion de la letra en letrasAbeced
+        int mayorRepeticiones = repetCadaLetra[0];
+        int cualLetra = 0;
+        //Recorre cada letra del texto
+        for (int i = 0; i < vchars.length; i++) {
+            
+            /*Recorre el vector letrasAbeced, si la letra del texto coincide con alguna
+            letra del vector, se suma 1 en repetCadaLetra (en la posición de la letra correspondiente).*/
+            for (int j = 0; j < letrasAbeced.length; j++) {
+                
+                if( vchars[i] == letrasAbeced[j] ){
+                    repetCadaLetra[j]++;
+                }
+            }
+        }
+        
+        for (int i = 1; i < repetCadaLetra.length; i++) {
+            if (repetCadaLetra[i] > mayorRepeticiones) {
+                mayorRepeticiones = repetCadaLetra[i];
+                cualLetra = i;
+            }
+        }        
+        return letrasAbeced[cualLetra];
     }
 }
